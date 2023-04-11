7800ddc725c9d90edb08654311e3518b6efd9c9e
adrjmz <jimenez.adrn@gmail.com>
2023-04-10 19:03:35 -0500

    0202 Happy Number

---
+++ leetcode/python/0202-happy-number.py
@@ -0,0 +1,22 @@
+# path: leeetcode\python\0202-happy-number.py
+# link: https://leetcode.com/problems/happy-number/
+# 2023-04-10
+# leetcode: Easy
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        slow, fast = n, self.sumSquareDigits(n)
+
+        while slow != fast:
+            fast = self.sumSquareDigits(fast)
+            fast = self.sumSquareDigits(fast)
+            slow = self.sumSquareDigits(slow)
+
+        return True if fast == 1 else False
+
+    def sumSquareDigits(self, n):
+        output = 0
+        while n:
+            output += (n % 10) ** 2
+            n = n // 10
+        return output
