83ac1a3d020c458957f223c91ff17d01b0150f5a
adrjmz <jimenez.adrn@gmail.com>
2023-04-10 19:12:02 -0500

    0049 Group Anagrams

---
+++ leetcode/python/0049-group-anagrams.py
@@ -0,0 +1,22 @@
+# path: leeetcode\python\0049-group-anagrams.py
+# link: https://leetcode.com/problems/group-anagrams/
+# 2023-04-10
+# leetcode: Medium
+
+import collections
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # 0(m * n)
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[tuple(count)].append(s)
+            
+        return res.values()
